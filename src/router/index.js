af71b49576c3b8afe974ad47a6aec6d751ebfb8d
Caique95Castro <124168022+Caique95Castro@users.noreply.github.com>
2024-05-21 17:23:04 -0300

    21-05

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+//import HomeView from '../views/HomeView.vue'
 
 const routes = [
+  //{
+  //  path: '/',
+  //  name: 'home',
+  //  component: HomeView
+  //},
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: () => import(/* webpackChunkName: "about" */ '../views/HomeView.vue')
   },
   {
     path: '/about',
